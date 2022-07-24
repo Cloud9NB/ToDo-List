e1bfd67df7a2dc8b366681830794999955a923b8
Cloud9NB <blackseeds10@hotmail.com>
2022-07-24 13:26:22 -0700

    passes state and have a mode if user is logged in or not

--- front-end/src/components/App.js
+++ front-end/src/components/App.js
@@ -1,12 +1,25 @@
 import Signup from './Signup';
 import Login from './Login';
 import TodoList from './TodoList';
+import useVisualMode from '../hooks/useVisualMode';
+import useApplicationData from '../hooks/hook';
 
 const App = () => {
+  const { state } = useApplicationData();
+
+  const LOGIN = 'LOGIN';
+  const SIGNUP = 'SIGNUP';
+  const SHOW = 'SHOW';
+
+  const { mode, transition, back } = useVisualMode(state.users ? SHOW : LOGIN);
+
   return (
-    // <Signup />
-    // <Login />
-    <TodoList />
+    <div>
+      <Signup />
+      {/* {mode === SIGNUP && <Signup />}
+      {mode === LOGIN && <Login />}
+      {mode === SHOW && <TodoList />} */}
+    </div>
   );
 };
 
