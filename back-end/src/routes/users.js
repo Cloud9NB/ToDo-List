fce38ba87383e38b35bfef4e337b22fb5cc583b1
Cloud9NB <blackseeds10@hotmail.com>
2022-07-24 13:26:52 -0700

    creates login and register routes

---
+++ back-end/src/routes/users.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+
+module.exports = db => {
+  router.post('/login', (req, res) => {
+    db.query(
+      `
+    SELECT *
+    FROM users
+    WHERE username = $1;
+    `,
+      [req.body.username]
+    )
+      .then(data => {
+        const users = data.rows;
+        res.json(users);
+      })
+      .catch(error => console.log('Error~~~', error));
+  });
+
+  router.post('/register', (req, res) => {
+    db.query(
+      `
+    INSERT INTO USERS (username, password)
+    VALUES ($1, $2);
+    `,
+      [req.body.username, req.body.password]
+    )
+      .then(data => {
+        const user = data.rows;
+        res.json(user);
+      })
+      .catch(error => console.log('Error~~~', error));
+  });
+
+  return router;
+};
