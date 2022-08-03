c1cf2fbd0cc1ac32ea26a3565b7a16bc97fe0881
Cloud9NB <blackseeds10@hotmail.com>
2022-08-03 12:39:14 -0700

    heroku

--- server.js
+++ server.js
@@ -14,15 +14,12 @@ app.use('/api', users());
 app.use('/api', tasks());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('view/build'));
+  app.use(express.static(path.join(__dirname, 'view/build')));
+
   app.get('*', (req, res) => {
     req.sendFile(path.resolve(__dirname, 'view/build', 'index.html'));
   });
 }
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'client/build', 'index.html'));
-});
 
 app.listen(port, () =>
   console.log(
